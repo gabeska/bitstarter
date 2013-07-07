7ebd404bf6e96497f5277d07c5d7f4fb91a6ce6b
Gabe Vader <gabevader@gmail.com>
2013-07-07 11:14:15 +0000

    added option to retrieve urls

--- grader.js
+++ grader.js
@@ -3,6 +3,8 @@
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -22,8 +24,8 @@ var cheerioHtmlFile = function (htmlfile) {
 var loadChecks = function (checksfile) {
 	return JSON.parse(fs.readFileSync(checksfile));
 };
-
-var checkHtmlFile = function (htmlfile, checksfile) {
+/*
+var checkHtmlFileOld = function (htmlfile, checksfile) {
 	var $ = cheerioHtmlFile(htmlfile),
 		checks = loadChecks(checksfile).sort(),
 		out = {};
@@ -33,22 +35,55 @@ var checkHtmlFile = function (htmlfile, checksfile) {
 	}
 	return out;
 };
+*/
+
+var checkHtmlFile = function (htmlfile, checksfile) {
+    var htmlString = fs.readFileSync(htmlfile);
+    return checkHtmlString(htmlString, checksfile);
+}
+
+var checkHtmlString = function (htmlString, checksfile) {
+    var $ = cheerio.load(htmlString);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+
+    for (var ii in checks) {
+	var present = $(checks[ii]).length>0;
+	out[checks[ii]] = present;
+    }
+    return out;
+
+};
 
 var clone = function (fn) {
 	// workaround for commander.js issue.
 	// http://stackoverflow.com/a/6772648
 	return fn.bind({});
 };
+var retrieveURLAndCheck = function(url, checksfile) {
+    // console.log('going to retrieve '+url);
+    restler.get(url).on('success', function(data, response) {
+	// console.log('success retrieving ' +url+ ' with checksfile ' + checksfile);
+	var checkJson = checkHtmlString(data.toString(), checksfile);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    });
+};
 
 if(require.main == module) {
-    console.log('hello there');
-	program
-		.option('-c, --checks <check_file>', "Path to checks.json", clone(assertFileExists), CHECKSFILE_DEFAULT)
-		.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)	
-		.parse(process.argv);
+    program
+	.option('-c, --checks <check_file>', "Path to checks.json", clone(assertFileExists), CHECKSFILE_DEFAULT)
+	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url>','URL to retrieve')
+	.parse(process.argv);
+
+    if(program.url) {
+	retrieveURLAndCheck(program.url, program.checks);
+    } else {
 	var checkJson = checkHtmlFile(program.file, program.checks);
 	var outJson = JSON.stringify(checkJson,null,4);
-	console.log(outJson);
+	console.log(outJson) ;
+    }
 } else {
-	exports.checkHtmlFile = checkHtmlFile;
+    exports.checkHtmlFile = checkHtmlFile;
 }
